c9af9c2939f948b136dfce9c7ba932d99aafbb77
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-07-12 18:39:34 -0500

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const db = requre("./models")
 
 const app = express();
 
